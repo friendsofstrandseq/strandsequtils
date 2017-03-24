91daabc8f8f8d0304b3ed1439ff1f0ed4d0c2c7e
Sascha Meiers <meiers@embl.de>
2017-03-24 14:45:02 +0100

    Fit line going through 0,0 because that is what we expect for the mean variance relationship in a NB

--- R/mean_var_correlation.R
+++ R/mean_var_correlation.R
@@ -14,11 +14,14 @@ d[, c("chrom", "cell", "sample") := .(
 setkey(d, sample, cell, chrom)
 
 e <- d[, .(mean = mean(w+c), var = var(w+c), median = median(w+c)), by = .(sample, cell)]
-e_len <- d[, .(len = length(unique(cell))), by = sample]
+e_len <- e[, .(len = length(unique(cell)),
+               p = sum(mean*mean) / sum(mean*var)), by = sample]
+
 plt <- ggplot( e ) +
     aes(mean, var) + 
     geom_point() +
     geom_smooth(method="lm") +
+    geom_abline(data = e_len, aes(intercept=0, slope = 1/p), col="darkorange", linetype="dashed", size=1.33) +
     ggtitle("Trio samples") +
     facet_wrap(~sample, ncol = 4) + 
     scale_x_continuous( limits=c(0, quantile(e$mean,0.98))) +
@@ -40,16 +43,17 @@ d[, c("chrom", "cell", "sample") := .(
 setkey(d, sample, cell, chrom)
 
 e <- d[, .(mean = mean(w+c), var = var(w+c), median = median(w+c)), by = .(sample, cell)]
-e_len <- d[, .(len = length(unique(cell))), by = sample]
+e_len <- e[, .(len = length(unique(cell)),
+               p = sum(mean*mean) / sum(mean*var)), by = sample]
+
 plt <- ggplot( e ) +
     aes(mean, var) + 
     geom_point() +
     geom_smooth(method="lm") +
+    geom_abline(data = e_len, aes(intercept=0, slope = 1/p), col="darkorange", linetype="dashed", size=1.33) +
     ggtitle("Chromothripsis samples") +
     facet_wrap(~sample, ncol = 4) + 
     scale_x_continuous( limits=c(0, quantile(e$mean,0.98))) +
     scale_y_continuous( limits=c(0, quantile(e$var,0.98))) +
     geom_label(data=e_len, x=0, y=Inf, hjust=0, vjust=1, aes(label = paste(len, "cells")))
 ggsave(plt, filename = "data/plots/chromothripsis.500k.q10.mean_var_corr.pdf", width = 10, height=8)
-
-
