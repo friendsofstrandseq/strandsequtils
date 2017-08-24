59705ddfd2c2932afeb9395769a26dfcf9de3b3c
Sascha Meiers <meiers@embl.de>
2017-08-24 13:37:54 +0200

    Corrected calculation of TP and FP

--- segmentation/scripts/calc_roc.R
+++ segmentation/scripts/calc_roc.R
@@ -13,60 +13,47 @@ colnames(bp) = c("k","bin","chrom")
 sv = fread(f_sv)
 colnames(sv) = c("chrom","start","end","type","vaf")
 
-
-# GRCh38
-chrom_sizes = data.table(
-    chrom = c(
-        "chr1",  "chr2",  "chr3",   "chr4",  "chr5",  "chr6",
-        "chr7",  "chr8",  "chr9",  "chr10", "chr11", "chr12",
-        "chr13", "chr14", "chr15", "chr16", "chr17", "chr18",
-        "chr19", "chr20", "chr21", "chr22", "chrX", "chrY"),
-    size = c(
-        248956422, 242193529, 198295559, 190214555,
-        181538259, 170805979, 159345973, 145138636,
-        138394717, 133797422, 135086622, 133275309,
-        114364328, 107043718, 101991189,  90338345,
-        83257441,  80373285,  58617616,  64444167,
-        46709983, 50818468, 156040895, 57227415))
-chrom_sizes$num_bins = as.integer(floor(chrom_sizes$size / windowsize))
-
-
-
-
-# Positions of SV breakpoints
-sv_coords = rbind(sv[, .(chrom, sv_pos = start)], sv[, .(chrom, sv_pos = end)])
-sv_coords = rbind(cbind(sv_coords, bin = floor(sv_coords$sv_pos/windowsize)),
-                  cbind(sv_coords, bin = ceiling(sv_coords$sv_pos/windowsize)))
-
 # Positions of breakpoints
 bp$bp_pos = bp$bin * windowsize
 
-# All bins
-all_bins = chrom_sizes[, data.table(bin = 0:num_bins), by = chrom]
-all_bins$pos = (all_bins$bin) * windowsize
-all_bins <- merge(all_bins, sv_coords, by = c("chrom", "bin"), all.x = T)
 
-# Annotate all bins by whether they match breakpoints and calculate ROC
-ROC <- NULL
+### NEW calculation of Sensitivity and Specificity
+summary = NULL
 for (k_ in unique(bp$k)) {
-    bkps <- merge(all_bins, bp[k==k_,], by = c("chrom", "bin"), all = T, allow.cartesian = T)
-    test <- bkps[, 
-                 .(total = length(pos),
-                   sv = sum(!is.na(sv_pos)),
-                   bp = sum(!is.na(bp_pos)),
-                   tp = sum(!is.na(sv_pos) & !is.na(bp_pos)),
-                   fp = sum(is.na(sv_pos) & !is.na(bp_pos))), 
-                 by = chrom][order(chrom),]
-    test <- test[, c("fp_rate","tp_rate") := .(fp / (total-sv), 2*tp/sv)][]
-    ROC <- rbind(ROC, cbind(test, k = k_))
+    
+    bp_k = bp[k == k_,]
+    bp_k$true = 0
+    
+    SVs = rbind(sv[, .(chrom, sv_pos = start)], sv[, .(chrom, sv_pos = end)])
+    SVs$found = 0
+    
+    for (i in 1:nrow(bp_k)) {
+        # true positives
+        # How many SV breakpoints were identified by (at least 1) breakpoint?
+        SVs[chrom == bp_k$chrom[i] & abs(sv_pos - bp_k$bp_pos[i]) <= 5*windowsize,]$found = 1
+        
+        # false positives
+        # How many of breakpoints are not close to SVs
+        if (nrow(SVs[chrom == bp_k$chrom[i] & abs(sv_pos - bp_k$bp_pos[i]) <= 5*windowsize,])>0) bp_k$true[i] = 1
+    }
+    summary_k = merge(SVs[, .(tp = sum(found)), by = chrom],
+          bp_k[, .(bps = .N, fp = .N - sum(true)), by = chrom],
+          by = "chrom")
+    summary = rbind(summary, cbind(summary_k, k=k_))
 }
 
-write.table(ROC, file = paste0(f_out, ".txt"), row.names = F, quote = F, col.names = T, sep = "\t")
-p <- ggplot(ROC[chrom == "chr1",]) + 
-    aes(fp_rate, tp_rate) + 
+# Add total number of 
+summary <- merge(summary, sv[, .(true = 2*.N), by = chrom], all.x=T)
+S = summary[chrom == "chr1", ]
+
+write.table(S, file = paste0(f_out, ".txt"), row.names = F, quote = F, col.names = T, sep = "\t")
+p <- ggplot(S) + 
+    aes(fp, tp/true) + 
     geom_line() + 
     geom_label(aes(label = k)) +
     ggtitle(paste("ROC curve", "chr1"))
+p
 ggsave(p, filename = f_out, width=8, height = 6)
+
 
 
