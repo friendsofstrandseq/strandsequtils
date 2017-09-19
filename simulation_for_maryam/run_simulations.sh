79a049d0c8a9539ff554b2fa16355631daca2ab9
Sascha Meiers <meiers@embl.de>
2017-09-19 09:48:12 +0200

    Running simulations for Maryam

--- simulation_for_maryam/run_simulations.sh
+++ simulation_for_maryam/run_simulations.sh
@@ -20,10 +20,20 @@ for vaf in 1 0.5 0.25 0.1
 do
     for size in small large
     do
-        for cov in 5 15
+        for cov in 5 15 45
         do
-            ls data/sv_config.vaf${vaf}.${size}.txt
-            ../../strseq/build/mosaic simulate -w 50000000 -n 100 -c $cov -C $cov -o data/counts.cov${cov}.vaf${vaf}.${size}.txt.gz data/sv_config.vaf${vaf}.${size}.txt
-        done
+            echo -e "Size ${size},\tVAF ${vaf},\tcov ${cov}"
+            ../../strseq/build/mosaic simulate -w 50000 -n 100 \
+                -c $cov -C $cov \
+                -o data/counts.cov${cov}.vaf${vaf}.${size}.txt.gz \
+                -V data/variants.cov${cov}.vaf${vaf}.${size}.txt \
+                -U data/breakpoints.cov${cov}.vaf${vaf}.${size}.txt \
+                data/sv_config.vaf${vaf}.${size}.txt
+            done
     done
 done
+
+
+echo "Plotting"
+#Rscript ../../strseq/R/qc.R data/sv_config.vaf${vaf}.${size}.txt data/qcplot.vaf${vaf}.${size}.pdf
+#Rscript ../../strseq/R/plot_segments.R data/counts.cov${cov}.vaf${vaf}.${size}.txt.gz data/variants.cov${cov}.vaf${vaf}.${size}.txt data/segmentplot.cov${cov}.vaf${vaf}.${size}.pdf
