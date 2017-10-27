4f16d369651e28f4d54663f57cf1984952e70f1a
maryamghr <maryam.ghareghani@gmail.com>
2017-10-27 11:16:00 +0200

    small change in cmd options

--- maryamCode/finalCall_cmd.R
+++ maryamCode/finalCall_cmd.R
@@ -80,7 +80,7 @@ stateFile = args[2,match("stateFile", as.character(args[1,]))]
 outputDir = args[2,match("outputDir", as.character(args[1,]))]
 bin.size = as.numeric(args[2,match("bin.size", as.character(args[1,]))])
 K = as.numeric(args[2,match("K", as.character(args[1,]))])
-maximumCN = as.numeric(args[2,match("maxCN", as.character(args[1,]))])
+maximumCN = as.numeric(args[2,match("maximumCN", as.character(args[1,]))])
 haplotypInfo=F
 if (any(as.character(args[1,])=="haplotypeInfo")){haplotypInfo = T}
 
@@ -90,9 +90,9 @@ print(paste("BRfile =", BRfile))
 print(paste("infoFile =", infoFile))
 print(paste("stateFile =", stateFile))
 print(paste("outputDir =", outputDir))
-print(paste("bin.size =", bin.size, class(bin.size)))
-print(paste("K =", K, class(K)))
-print(paste("maximumCN =", maximumCN, class(maximumCN)))
+print(paste("bin.size =", bin.size))
+print(paste("K =", K))
+print(paste("maximumCN =", maximumCN))
 
 binRC = split.chromosomes(changeRCformat(binRCfile, outputDir))
 cellTypes = changeCellTypesFormat(stateFile)
