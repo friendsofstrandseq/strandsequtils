53007ec90b131945344991da4874dd3332b1dc7c
Tobias Marschall <tobias.marschall@0ohm.net>
2018-01-10 13:51:09 +0100

    Remove Maryam's package. It is now in a separate repository: https://github.com/friendsofstrandseq/MaRyam

--- maryamCode/runScriptSimulated.R
+++
@@ -1,7 +0,0 @@
-directory = "/local/data/maryam/codes/strand-seq/SVcalling/simulatedData/dataset2/newFormat/"
-
-for (dir in system(paste("ls", directory), intern = T))
-{
-  system(paste0("mkdir ",directory, dir, "/NBfitPlots"))
-  system(paste0("Rscript runCalssificationOnSimulatedData.R ", directory, dir, "/ &disown"))
-}
